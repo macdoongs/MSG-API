a62589ca8a94de66c1063634c6aa7447c1ef9fbb
Youngmin Kim <rladudals02@naver.com>
2016-07-09 22:40:01 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,163 @@
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var favicon = require('serve-favicon');
+//global.jQuery = require('jQuery');
+//var bootstrap_jQuery=('bootstrap-jquery');
+//var bootstrap = require('bootstrap');
+//var node_jsdom = require('node-jsdom');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'image')));
+
+
+var cheerio = require('cheerio');
+//var request = require('request');
+
+var config = require('config.json')('./config/config.json');
+
+console.log("Webpage start!");
+
+var fs = require('fs');
+
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+	host      : config.rds.host,
+	user      : config.rds.user,
+	password  : config.rds.password,
+	database  : config.rds.webdatabase
+});
+
+conn.connect();
+
+var pug = require('pug');
+
+//app.set('view engine', 'jade')
+//app.set('views', path.join(__dirname, '/views'));
+
+
+//var url = 'http://www.tistory.com';
+//request(url, function(error, response, html){
+//	if (error) {throw error};
+
+	//console.log (html);
+
+//	var $ = cheerio.load(html);
+
+//	var arr = $('.tistory_recomm').children('.recomm_blog').children('.tit_subject');
+  	//for(var i=0;i<arr.prevObject.length;i++){
+  	//	res.send(arr.prevObject[i].children[0].next.children[3].children[0].data);
+	//}
+//});
+
+
+
+/*
+//var sql = 'INSERT INTO topic (title, description, author) VALUE (?, ?, ?)';
+
+//var params = {'Supervisor', 'Watcher', 'gra'};
+
+// conn.query(sql, params, function(err, rows, fields){
+//   if(err){
+//     throw err;
+//   } else{
+//     console.log('rows', rows);
+//     console.log('fields', fields);
+//   }
+// });
+
+// var sql = 'SELECT id, title, author, created FROM topic';
+//
+// conn.query(sql, function(err, rows, fields){
+//   if(err){
+//     console.log(err);
+//   } else{
+//     console.log('rows', rows);
+//     console.log('fields', fields);
+//   }
+// });
+*/
+
+//app.use('/image', './image');
+
+app.locals.pretty = true;
+
+app.set('view engine', 'html');
+//app.set('views', './views_mysql');
+//app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, './views'));
+
+
+
+var server = app.listen(config.app.port, function(){
+	console.log('Express server has started on port ' + config.app.port + ' !');
+
+	if( process.env.NODE_ENV == 'production' ) {
+    		console.log("Production Mode");
+  	} else if( process.env.NODE_ENV == 'development' ) {
+    		console.log("Development Mode");
+  	}
+});
+
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
+/*
+app.get(['/topic', '/topic/:id'],function(req, res){
+
+  var sql = 'SELECT id, title FROM topic';
+
+  conn.query(sql, function(err, topics, fields){
+    var id = req.params.id;
+    if(id){
+      var sql = 'SELECT * FROM topic WHERE id=?';
+      conn.query(sql, [id], function(err, topic, fields){
+        if(err){
+          console.log(err);
+        }else{
+          res.render('view', {topics:topics, topic:topic[0]});
+        }
+      });
+    }else{
+      res.render('view', {topics:topics});
+    }
+  });
+});
+*/
+
+app.get('/', function(req, res){
+
+/*
+
+	//res.render('index', { title: 'Korchid' , name:'uiandwe'});
+	var sql = 'SELECT id, title FROM topic';
+
+	conn.query(sql, function(err, topics, fields){
+    		var id = req.params.id;
+		var title = req.params.title;
+
+    		if(id){
+      			var sql = 'SELECT * FROM topic WHERE id=?';
+      			conn.query(sql, [id], function(err, topic, fields){
+        			if(err){
+          				console.log(err);
+        			}else{
+          				console.log(id);
+        			}
+      			});
+    		}else{
+    			console.log("miss DB data");
+		}
+  	});
+*/
+	//pug.render('index');
+	res.sendFile(path.join(__dirname + '/views/index.html'));
+	//res.sendFile('/index.html');
+
+
+});
+
+
+conn.end();
