395ea13c165197b021c7ae56e2bbcf8b7c9fd3b9
Youngmin Kim <rladudals02@naver.com>
2016-07-12 13:50:24 +0000

    MQTT

--- routes/iot.js
+++ routes/iot.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var config = require('config.json')('./config/config.json');
 
+var util = require('util');
+
+var mqtt = require('mqtt');
+
 var mysql = require('mysql');
 var conn = mysql.createConnection({
 	host      : config.rds.host,
@@ -17,6 +21,7 @@ router.get(['/', '/:id'], function(req, res, next) {
   var myName = config.rds.user;
 	var topicId = req.params.id;
 	var topicSeq = null;
+	var mqtt_client = config.iot.mqttproxy;
 
   var sql = 'SELECT * FROM cnt';
 
@@ -35,8 +40,10 @@ router.get(['/', '/:id'], function(req, res, next) {
 							topicSeq = i;
 					}
 				}
+				var message = mqtt_req_connect(mqtt_client);
 
-				res.render('iot', { title: 'KYM coms', rows: pRows, id:topicId });
+
+				res.render('iot', { title: 'KYM coms', rows: pRows, id:topicId , message : message});
 
 			}
 		}
@@ -44,5 +51,28 @@ router.get(['/', '/:id'], function(req, res, next) {
 
 });
 
+
+function mqtt_req_connect(brokerip) {
+		var usemqttaeid = config.iot.mqttaeid;
+    var req_topic = util.format('/oneM2M/req/+/%s/#', usemqttaeid);
+    var mqtt_client = mqtt.connect('mqtt://' + brokerip);
+
+    mqtt_client.on('connect', function () {
+        mqtt_client.subscribe(req_topic);
+
+				var message = "connect"
+
+				console.log(message);
+
+				return message;
+    });
+
+    mqtt_client.on('message', function (topic, message) {
+        //var topic_arr = topic.split("/");
+				console.log(message);
+
+				return message;
+    });
+}
 
 module.exports = router;
