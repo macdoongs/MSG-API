f6deb4d725efe78cfa3f1271b5c85f00abfe9d9a
Youngmin Kim <rladudals02@naver.com>
2016-07-14 14:51:14 +0000

    iot page

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'jquery-mobile')));
 app.use(express.static(path.join(__dirname, 'bootstrap-3.3.4-dist')));
 //app.use(express.static(path.join(__dirname, 'bootstrap-3.3.4-dist/css')));
 //app.use(express.static(path.join(__dirname, 'bootstrap-3.3.4-dist/fonts')));
@@ -57,6 +58,21 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+
+// // 3033 포트로 소켓을 연다
+// var io = require('socket.io').listen(3033);
+//
+// // connection이 발생할 때 핸들러를 실행한다.
+// io.sockets.on('connection', function (socket) {
+// // 클라이언트로 news 이벤트를 보낸다.
+//     socket.emit('news', { hello: 'world' });
+//
+// // 클라이언트에서 my other event가 발생하면 데이터를 받는다.
+// socket.on('my other event', function (data) {
+//         console.log(data);
+//     });
+// });
 
 // error handlers
 
--- routes/iot.js
+++ routes/iot.js
@@ -15,7 +15,20 @@ var conn = mysql.createConnection({
 });
 
 conn.connect();
-
+//
+// // 3033 포트로 소켓을 연다
+// var io = require('socket.io').listen(3033);
+//
+// // connection이 발생할 때 핸들러를 실행한다.
+// io.sockets.on('connection', function (socket) {
+// // 클라이언트로 news 이벤트를 보낸다.
+//     socket.emit('news', { hello: 'world' });
+//
+// 		// 클라이언트에서 my other event가 발생하면 데이터를 받는다.
+// 	socket.on('my other event', function (data) {
+//         console.log(data);
+//     });
+// });
 
 var sh_timer = require('./timer');
 
@@ -47,48 +60,67 @@ router.get(['/', '/:id'], function(req, res, next) {
 
 				var message = "hi";
 
-				res.render('iot', { title: 'KYM coms', rows: pRows, id:topicId , message : message});
+				res.render('iot', { title: 'MMCN 2016', rows: pRows, id:topicId , message : message});
 
 			}
 		}
   });
 
 });
+//
+ global.subCount = 0;
+// var usemqttaeid = config.iot.mqttaeid;
+// var req_topic = util.format('/oneM2M/req/+/%s/#', usemqttaeid);
+// var mqtt_client = mqtt.connect('mqtt://' + config.iot.mqttproxy);
 
-var count = 0;
-var usemqttaeid = config.iot.mqttaeid;
-var req_topic = util.format('/oneM2M/req/+/%s/#', usemqttaeid);
-var mqtt_client = mqtt.connect('mqtt://' + config.iot.mqttproxy);
-
-
-//if(count == 0){
-mqtt_client.on('connect', function () {
-		mqtt_client.subscribe(req_topic);
-
-		console.log(req_topic);
-
-		var message = "Connect";
-
-		console.log(message);
-
-		return message;
-});
-//}
+//
+// mqtt_client.on('connect', function () {
+// 		mqtt_client.subscribe(req_topic);
+//
+// 		console.log(req_topic);
+//
+// 		var message = "Connect";
+//
+// 		console.log(message);
+//
+// 		return message;
+// });
+//
+//
+// mqtt_client.on('message', function (topic, message) {
+// 		//var topic_arr = topic.split("/");
+// 		console.log(message.toString());
+//
+// 		var mqtt_client = config.iot.mqttproxy;
+//
+// 	  var sql = 'SELECT * FROM cnt';
+//
+// 	  conn.query(sql, function(err, rows, fields){
+// 	  	if(err){
+// 	  		console.log(err);
+// 	  	}else{
+// 	  		if(!rows.length){
+// 	  			console.log("Missing DB data.");
+// 	  		}else{
+// 					var sRows = JSON.stringify(rows);
+// 					var pRows = JSON.parse(sRows);
+//
+// 					console.log(message);
+//
+// 					io.emit('news', { hello: message });
+// 				}
+// 			}
+// 	  });
+// });
 
 sh_timer.timer.on('tick', function() {
-		count++;
+		subCount++;
 
-		console.log(count);
+		console.log(subCount);
 
 		//if(count >= 100){
 			//count = 0;
 
-
-
-	      	mqtt_client.on('message', function (topic, message) {
-	        //var topic_arr = topic.split("/");
-					console.log(message);
-	    });
 		//}
 });
 
