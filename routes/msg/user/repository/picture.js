496cd19b20ba17751450ebee6e82004c89e8bdbc
Youngmin Kim <rladudals02@gmail.com>
2017-02-01 01:44:19 +0900

    Add multiparty

--- routes/msg/user/repository/picture.js
+++ routes/msg/user/repository/picture.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 var config = require('config.json')('./config/config.json');
 
+var multiparty = require('multiparty');
+var fs = require('fs');
+
 var repository_model = require('../../../../models/msg/repository.model');
 
 var db = require('../../../../models/msg/db_action');
@@ -20,9 +23,53 @@ db.connect(host, port, user, password, database, function(callback){
 	}
 });
 
-router.post(['/:loginToken'], function(req, res, next){
+router.post(['/'], function(req, res, next){
 
+	var form = new multiparty.Form();
 
+  // get field name & value
+  form.on('field',function(name,value){
+       console.log('normal field / name = '+name+' , value = '+value);
+  });
+
+  // file upload handling
+  form.on('part',function(part){
+       var filename;
+       var size;
+       if (part.filename) {
+             filename = part.filename;
+             size = part.byteCount;
+       }else{
+             part.resume();
+
+       }
+
+       console.log("Write Streaming file :"+filename);
+       var writeStream = fs.createWriteStream('/tmp/'+filename);
+       writeStream.filename = filename;
+       part.pipe(writeStream);
+
+       part.on('data',function(chunk){
+             console.log(filename+' read '+chunk.length + 'bytes');
+       });
+
+       part.on('end',function(){
+             console.log(filename+' Part read complete');
+             writeStream.end();
+       });
+  });
+
+  // all uploads are completed
+  form.on('close',function(){
+       res.status(200).send('Upload complete');
+  });
+
+  // track progress
+  form.on('progress',function(byteRead,byteExpected){
+       console.log(' Reading total  '+byteRead+'/'+byteExpected);
+  });
+
+  form.parse(req);
 
 
   res.send("OK");
